04a37a3106826228d07a5487bbb9058f5805a7e9
rsysganeshpatwa <ganesh.patwa@rsystems.com>
2025-02-11 11:53:09 +0530

    media package pluigin added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const { HLSPullPush, S3BucketOutput } = require("@eyevinn/hls-pull-push");
 // Initialize the service
 const pullPushService = new HLSPullPush();
 pullPushService.registerPlugin("s3", new S3BucketOutput());
+pullPushService.registerPlugin("mediapackage", new MediaPackageOutput());
 pullPushService.listen(process.env.PORT || 8080);
 
 console.log("HLS Pull-Push service running!");
